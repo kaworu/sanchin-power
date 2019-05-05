ab88bf649f0605e0c2158e45e4f12e0eab2fd321
Alexandre Perrin <alex@kaworu.ch>
2019-05-05 21:57:54 +0200

    Regexp typo fix

--- app/lib/regexp.rb
+++ app/lib/regexp.rb
@@ -4,6 +4,6 @@ module Sanchin
   # Some commonly used Regexp.
   module Regexp
     UUID_V4 = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.freeze
-    BCRYPT_HASH = /^\$[0-9a-z]{2}\$[0-9]{2}\$[A-Za-z0-9./]{53}$/.freeze
+    BCRYPT_HASH = /^\$[0-9a-z]{2}\$[0-9]{2}\$[A-Za-z0-9.\/]{53}$/.freeze
   end
 end
